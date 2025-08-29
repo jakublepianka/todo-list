18fc6245e3b652e041d5f4c4d5ebeccaf389f1b9
resq-u <jakub12lepianka@wp.pl>
2025-08-29 12:01:18 +0200

    Add functionalities, change code structure, add icons

--- src/DOM/projectDOM.js
+++ src/DOM/projectDOM.js
@@ -1,12 +1,11 @@
 import { projectModel } from "../Models/projectModel";
+import { todoDOM } from "./todoDOM";
+import viewImage from "../assets/icons/book.png"
+import arrowImage from "../assets/icons/up-arrow.png"
+import plusImage from "../assets/icons/plus.png"
+import editImage from "../assets/icons/edit.png"
 
 export const projectDOM = (function(){
-
-    // const newProj = projectModel.createDefaultProject();
-    // const newtodoer = newProj.createNewTodo('habibi', 'origami');
-    // console.log(newtodoer);
-    // newProj.addTodo(newtodoer);
-
 
     const content = document.querySelector('#content');
     const contentHeader = document.createElement('div');
@@ -17,27 +16,49 @@ export const projectDOM = (function(){
     contentTitle.classList.add('content-title');
     contentBody.classList.add('content-body');
 
-
-    function addProjectsElements(){
+    function loadBaseProjectElements(){
 
         contentTitle.textContent = 'Your Projects';
+
 
         content.appendChild(contentHeader);
         contentHeader.appendChild(contentTitle);
         content.appendChild(contentBody);
-
+        if(contentHeader.getElementsByClassName('clear-button').length !== 0) return;
+        createAddProjectButton()
+        addClearButton();
     } 
 
-    function addNewProjectCard(newProject){
+    function addClearButton(){
+
+        const clearButton = document.createElement('button');
+        
+        clearButton.classList.add('clear-button');
+
+        clearButton.textContent = 'Delete all projects';
+
+        clearButton.addEventListener("click", () => {
+            
+            projectModel.deleteAllProjects();
+            contentBody.replaceChildren();
+            createAddProjectButton();
+            clearButton.remove();
+
+        });
+
+        contentHeader.appendChild(clearButton);
+
+    }
+
+    function loadProjectCard(newProject){
 
         const projectCard = document.createElement('div');
         const projectHeader = document.createElement('div');
         const projectTitle = document.createElement('h3');
-        
 
         const projectBody = document.createElement('div');
+
         const projectDescriptionContainer = document.createElement('div');
-        const projectDescriptionText = document.createElement('h4');
         const projectTodolistContainer = document.createElement('div');
 
         projectCard.classList.add('project-card');
@@ -45,48 +66,207 @@ export const projectDOM = (function(){
         projectTitle.classList.add('project-title');
         projectBody.classList.add('project-body');
 
-
         projectDescriptionContainer.classList.add('project-description-container');
-        projectDescriptionText.classList.add('project-description-text');
         projectTodolistContainer.classList.add('project-todolist-container');
 
         projectTitle.textContent = newProject.projectName;
 
+        function loadDescriptionForm() {
+
+            const descriptionForm = document.createElement('div');
+            const textArea = document.createElement('textarea');
+    
+            descriptionForm.classList.add('description-form');
+            textArea.classList.add('description-textarea');
+            
+            textArea.style.width = '100%';
+            
+    
+            if (newProject.description === '') { 
+                textArea.placeholder = 'Project\'s Description here!';
+            } else {
+                textArea.textContent = newProject.projectDescription;
+            }
+    
+            textArea.addEventListener("input", () => {
+                    textArea.style.height = "auto";
+                    textArea.style.height = textArea.scrollHeight + 16 + "px";
+            });
+    
+            const createSubmitButton = () => {
+    
+                const submitBtn = document.createElement('button');
+                submitBtn.classList.add('description-submit-button');
+                submitBtn.textContent = 'Confirm';
+    
+                submitBtn.addEventListener("click", (event) => {
+    
+                    event.target.parentNode.remove();                            
+                    newProject.editDescription(textArea.value);
+                    loadDescription();
+    
+                });
+                return submitBtn;
+            }
+                
+            const createCancelButton = () => {
+                
+                const cancelBtn = document.createElement('button');
+                cancelBtn.classList.add('description-cancel-button');
+                cancelBtn.textContent = 'Cancel';
+    
+                cancelBtn.addEventListener("click", (event) => {
+    
+                    event.target.parentNode.remove();
+                    loadDescription();
+    
+                });
+    
+                return cancelBtn;
+            }
+                
+            projectDescriptionContainer.appendChild(descriptionForm);
+            descriptionForm.appendChild(textArea);
+            descriptionForm.appendChild(createSubmitButton());
+            descriptionForm.appendChild(createCancelButton());
+    
+            textArea.style.height = "auto";
+            textArea.style.height = textArea.scrollHeight + 16 + "px";
+
+        }
+
+        const createDescriptionEditButton = () => {
+
+            const editBtn = document.createElement('button');
+            const editIcon = document.createElement('img');
+
+            editBtn.classList.add('description-edit-button');
+            editIcon.classList.add('description-edit-icon');                    
+
+            editBtn.type = 'submit';
+            editIcon.src = editImage;
+
+            editBtn.addEventListener("click", (event) => {
+
+                const parentElement = event.currentTarget.parentNode;
+
+                parentElement.remove();
+                loadDescriptionForm();
+                return;
+
+            });
+
+            editBtn.appendChild(editIcon);
+            
+            return editBtn;
+
+        };
+
+        function loadDescription(){
+
+            if (projectDescriptionContainer.getElementsByClassName('project-description-wrapper').length > 0) return;
+            const projectDescriptionWrapper = document.createElement('div');
+            const projectDescriptionText = document.createElement('h4');
+
+            projectDescriptionText.classList.add('project-description-text');
+            projectDescriptionWrapper.classList.add('project-description-wrapper');
+
+            projectDescriptionText.textContent = newProject.projectDescription;
+
+            projectBody.appendChild(projectDescriptionContainer);
+            projectDescriptionContainer.appendChild(projectDescriptionWrapper);
+            projectDescriptionWrapper.appendChild(projectDescriptionText);
+            projectDescriptionWrapper.appendChild(createDescriptionEditButton());
+
+
+            return projectDescriptionContainer;
+        }
+
+        function loadTodolist(project) {
+
+            const todoList = document.createElement('ul');
+    
+            todoList.classList.add('project-todolist');
+    
+            if(projectTodolistContainer.getElementsByClassName('project-todolist').length > 0) return;
+            projectTodolistContainer.appendChild(todoList);
+    
+            project.projectTodos.forEach(todo => {
+                const item = document.createElement('li');
+                const titleSpan = document.createElement('span');
+                const dueDateSpan = document.createElement('span');
+                const prioritySpan = document.createElement('span');
+    
+                item.classList.add('project-todo-item');
+                titleSpan.classList.add('project-todo-title');
+                dueDateSpan.classList.add('project-todo-date');
+                prioritySpan.classList.add('project-todo-priority');
+                
+                titleSpan.textContent = todo.title;
+                dueDateSpan.textContent = todo.dueDate;
+                prioritySpan.textContent = todo.priority;
+    
+                todoList.appendChild(item);
+                item.appendChild(titleSpan);
+                item.appendChild(dueDateSpan);
+                item.appendChild(prioritySpan);
+    
+            });
+        }
+
         const createExpandBtn = () => {
 
             const expandBtn = document.createElement('button');
-            expandBtn.classList.add('project-expand');
-            expandBtn.textContent = 'V';
+            const expandIcon = document.createElement('img');
+
+            expandBtn.classList.add('project-expand-button');
+            expandIcon.classList.add('project-expand-icon')
+
+            expandIcon.src = arrowImage;
+            expandIcon.style.transform = "rotate(-180deg) translateY(-4px)";
+
             let isClicked = false;
     
             expandBtn.addEventListener("click", () => {
                 isClicked = !isClicked;
                 if (isClicked) {
                     
-                    projectDescriptionText.textContent = newProject.projectDescription;
-                    loadTodolist(newProject, projectTodolistContainer);
+                    expandIcon.style.transform = '';
+
+                    loadDescription();
+                    projectBody.appendChild(projectTodolistContainer);
+                    loadTodolist(newProject);
                     
                 } else {
 
-                    projectDescriptionContainer.classList = 'project-description-container';
-                    projectDescriptionText.textContent = '';
+                    expandIcon.style.transform = "rotate(-180deg) translateY(-4px)";
 
+                    projectDescriptionContainer.replaceChildren();
                     projectTodolistContainer.replaceChildren();
                 }
             });
         
+            expandBtn.appendChild(expandIcon);
+
             return expandBtn;
         };
 
         const createOpenBtn = () => {
             const projectOpen = document.createElement('button');
-            projectOpen.classList.add('project-open');
-            projectOpen.textContent = 'O';   
+            const projectOpenIcon = document.createElement('img');
+
+            projectOpen.classList.add('project-open-button');
+            projectOpenIcon.classList.add('project-open-icon');
+
+            projectOpenIcon.src = viewImage;
+            projectOpenIcon.style.transform = "translateY(4px)";
 
             projectOpen.addEventListener("click", () => {
-                
+                content.replaceChildren();
+                todoDOM.loadProjectsTodoCards(newProject);
             });
 
+            projectOpen.appendChild(projectOpenIcon);
             return projectOpen;
         };
 
@@ -97,78 +277,61 @@ export const projectDOM = (function(){
         
         projectHeader.appendChild(projectTitle);
         projectHeader.appendChild(createExpandBtn());
-        projectHeader.appendChild(createOpenBtn());
-        
-        projectBody.appendChild(projectDescriptionContainer);
-        projectBody.appendChild(projectTodolistContainer);
-
-        projectDescriptionContainer.appendChild(projectDescriptionText);
+        projectHeader.appendChild(createOpenBtn());        
     
     }
-    
 
     function addDefaultProjectCard(){
-        addNewProjectCard(projectModel.createDefaultProject());
+        loadProjectCard(projectModel.createDefaultProject());
     }
 
-    function loadTodolist(project, targetEl) {
 
-        const todoList = document.createElement('ul');
 
-        todoList.classList.add('project-todolist');
-
-        targetEl.appendChild(todoList);
-
-        project.projectTodos.forEach(todo => {
-            const item = document.createElement('li');
-            const title = document.createElement('span');
-            const dueDate = document.createElement('span');
-            const priority = document.createElement('span');
-
-            item.classList.add('project-todo-item');
-            title.classList.add('project-todo-title');
-            dueDate.classList.add('project-todo-date');
-            priority.classList.add('project-todo-priority');
-
-            title.textContent = todo.todoTitle;
-            dueDate.textContent = todo.todoDueDate;
-            priority.textContent = todo.todoPriority;
-
-            todoList.appendChild(item);
-            item.appendChild(title);
-            item.appendChild(dueDate);
-            item.appendChild(priority);
-
+    function loadAllExistingProjects(projectsList){
+        if (contentBody.getElementsByClassName('project-card').length !== 0) return;
+        projectsList.forEach(project => {
+            loadProjectCard(project);
         });
-
     }
 
-    const addNewProjectButton = () => {
+    function loadEverything(){
+        loadBaseProjectElements();
+        loadAllExistingProjects(projectModel.projectsList);
+    }
+
+    const createAddProjectButton = () => {
         
         const addProjectBtn = document.createElement('button');
-        const icon = document.createElement('p');
+        const addIcon = document.createElement('img');
+        const btnText = document.createElement('p');
         
         addProjectBtn.classList.add('project-add');
-        icon.classList.add('project-add-icon');
-        icon.textContent = '+';
+        addIcon.classList.add('project-add-icon');
+        btnText.classList.add('project-add-text');
+
+        addIcon.src = plusImage;
 
         contentBody.appendChild(addProjectBtn);
-        addProjectBtn.appendChild(icon);
+        addProjectBtn.appendChild(addIcon);
+        addProjectBtn.appendChild(btnText);
 
         addProjectBtn.addEventListener("click", () => {
             if (contentBody.getElementsByClassName('form-card').length >= 1) {
-                icon.classList = '';
-                icon.classList.add('project-add-icon-info');        
-                icon.textContent = 'Form is ready';
+                addIcon.src = '';
+                
+                if(btnText.textContent) return;
+                btnText.textContent = 'Form is ready';
+
                 setTimeout(() => {
-                    icon.classList = '';
-                    icon.classList.add('project-add-icon');
-                    icon.textContent = '+';               
+                    btnText.textContent = ''
+                    addIcon.src = plusImage;
                 }, 1500);
 
                 return;
+
             }
-            
+
+            if(contentHeader.getElementsByClassName('clear-button').length === 0) addClearButton();
             addNewProjectForm();
         });
 
@@ -196,7 +359,6 @@ export const projectDOM = (function(){
 
         const descriptionLabel = document.createElement('label');
         const descriptionInput = document.createElement('textarea');
-        const descriptionError = document.createElement('span');
 
         const formSubmitContainer = document.createElement('div');
 
@@ -211,6 +373,7 @@ export const projectDOM = (function(){
 
         nameLabel.setAttribute("for", "project_name");
         nameInput.setAttribute("id", "project_name");
+        nameInput.type = 'text';
 
         descriptionLabel.setAttribute("for", "project_description");
         descriptionInput.setAttribute("id", "project_description");
@@ -234,8 +397,10 @@ export const projectDOM = (function(){
 
                 nameError.textContent = '';
                 const submittedProject = projectModel.createProject(nameInput.value, descriptionInput.value);
+                projectModel.addProjectToProjectsList(submittedProject);
                 formCard.remove();                
-                addNewProjectCard(submittedProject);
+                loadProjectCard(submittedProject);
+                console.log(projectModel.projectsList);
             
             });
         
@@ -271,7 +436,6 @@ export const projectDOM = (function(){
 
         formDescriptionItem.appendChild(descriptionLabel);
         formDescriptionItem.appendChild(descriptionInput);
-        formDescriptionItem.appendChild(descriptionError);
 
         formContainer.appendChild(formSubmitContainer);
         formSubmitContainer.appendChild(addSubmitFormButton());
@@ -279,36 +443,11 @@ export const projectDOM = (function(){
     
     };
 
-    const deleteExpandBtn = () => {
-
-
-    };
-
-    const createShrinkBtn = () => {
-
-    };
-
-
-
-
-       // function addSidebarLayout() {
-       
-    //     const sidebar = document.createElement('div');
-    //     const sidebarHeader = document.createElement('div');
-    //     const sidebarHeaderTitle = document.createElement('h2');
-        
-    // }
-    
-    
-
-
-
-
     return {
-        addProjectsElements,
-        addNewProjectCard,
+        loadBaseProjectElements,
+        loadProjectCard,
         addDefaultProjectCard,
-        addNewProjectButton,
-        addNewProjectForm
+        addNewProjectForm,
+        loadEverything
     }
 })();

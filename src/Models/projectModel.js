eb1e165edbb7fe32d66ea0cebc4b08596325af88
resq-u <jakub12lepianka@wp.pl>
2025-08-29 11:12:40 +0200

    Add functions to manage project data

--- src/Models/projectModel.js
+++ src/Models/projectModel.js
@@ -6,9 +6,11 @@ import { todoModel } from "./todoModel.js"
 
 export const projectModel = (function(){
 
+    const projectsList = [];
+
     const createProject = function(name, description = '') {
     
-        // const projectId = crypto.randomUUID();
+        const projectId = crypto.randomUUID();
         let projectName = name;
         let projectDescription = description;
         const projectTodos = [];
@@ -17,9 +19,10 @@ export const projectModel = (function(){
         const editDescription = (newDescription) => { projectDescription = newDescription; };
         const addTodo = (newTodo) => { projectTodos.push(newTodo); };
         const createNewTodo = (title, description) => { return todoModel.createTodo(title, description); };
-        const deleteTodo = (todoObj) => { projectTodos[projectTodos.indexOf(todoObj)] };
+        const deleteTodo = (todoId) => { projectTodos.splice(projectTodos.indexOf(todoId), 1); };
 
         return {
+            get projectId() { return projectId; },
             get projectName() { return projectName; },
             get projectDescription() { return projectDescription; },
             get projectTodos() { return projectTodos; },
@@ -32,19 +35,44 @@ export const projectModel = (function(){
     }
 
     function createDefaultProject(){
-        const defaultProject = createProject('Your Project, but with a slightly longer title', 'Your first project');
-        const defaultTodo = defaultProject.createNewTodo('To do a habibi');
+        
+        const defaultProject = createProject('Your Project, but the this time the title is long', 'Your first project');
+        const defaultTodo = defaultProject.createNewTodo('To do a habibi', 'Here i would like to place a somewhat detailed description of a task');
+
         defaultTodo.editDueDate(new Date(2026, 11, 31));
         defaultTodo.editPriority(3);
-        defaultProject.projectTodos.push(defaultTodo);
+        defaultTodo.editNotes('To do a habibi and remberaaaaaaaaaaaaaaaaaaa uuuuuuu it\'s a very long note');
+        defaultTodo.createCheckbox('before habibi to do origami');
+        defaultTodo.createCheckbox('After origami do a girobi');
+        
+        addTodoToProjectsTodos(defaultProject.projectTodos, defaultTodo);
+        addProjectToProjectsList(defaultProject);
         return defaultProject;
     }
 
+    function addProjectToProjectsList(project){
+        projectsList.push(project);
+    }
 
+    function addTodoToProjectsTodos(projectsTodos, todo){
+        projectsTodos.push(todo);
+    }
+
+    function deleteProjectFromProjectsList(project){
+        projectsList.splice(projectsList.indexOf(project), 1);
+    }
+
+    function deleteAllProjects(){
+        projectsList.splice(0, projectsList.length);
+    }
 
     return {
+        get projectsList() { return projectsList },
         createProject,
         createDefaultProject,
+        deleteProjectFromProjectsList,
+        deleteAllProjects,
+        addProjectToProjectsList,
     }
 
 })();
